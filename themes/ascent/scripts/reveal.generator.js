81d6767d14a4a3174fd60eebc4d5bee814a4ec2e
yqwoe <yqwoe@live.cn>
2026-04-09 10:49:22 +0800

    fix:haikang detail未关闭；

--- themes/ascent/scripts/reveal.generator.js
+++ themes/ascent/scripts/reveal.generator.js
@@ -1,34 +1,77 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.revealGeneratorCallback = void 0;
-const fs_1 = require("fs");
-const process_1 = require("process");
-const glob_1 = require("glob");
-const revealGeneratorCallback = () => {
-    const dirs = [
-        __dirname + '/../node_modules/reveal.js',
-        (0, process_1.cwd)() + '/node_modules/reveal.js',
-    ];
-    const dir = dirs.find((dir) => (0, fs_1.existsSync)(dir));
-    if (!dir) {
-        throw new Error('reveal.js not found');
-    }
-    const baseDir = (0, fs_1.realpathSync)(dir);
+'use strict';
 
-    const files = [
-        ...(0, glob_1.sync)(baseDir + '*/dist/**/*', { nodir: true }),
-        ...(0, glob_1.sync)(baseDir + '*/plugin/**/*', { nodir: true }),
-        ...(0, glob_1.sync)(baseDir + '*/*', { nodir: true }),
-        ...(0, glob_1.sync)(baseDir + '*/**/*', { nodir: true }),
-        ...(0, glob_1.sync)((0, process_1.cwd)() + '/node_modules/revealjs' + '*/**/*', { nodir: true }),
-    ]
-        .map((file) => {
-            return {
-                data: () => (0, fs_1.createReadStream)(file),
-                path: file.split('/node_modules/')[1],
-            }
-        });
-    // console.log(files)
-    return files;
+const fs = require('fs');
+const path = require('path');
+const { cwd } = require('process');
+
+function listFilesRecursive(rootDir) {
+  const out = [];
+  function walk(dir) {
+    let entries;
+    try {
+      entries = fs.readdirSync(dir, { withFileTypes: true });
+    } catch {
+      return;
+    }
+    for (const ent of entries) {
+      const full = path.join(dir, ent.name);
+      if (ent.isDirectory()) {
+        walk(full);
+      } else if (ent.isFile()) {
+        out.push(full);
+      }
+    }
+  }
+  if (fs.existsSync(rootDir)) {
+    walk(rootDir);
+  }
+  return out;
+}
+
+function pathUnderNodeModules(file) {
+  const parts = file.split(path.sep);
+  const nmIdx = parts.lastIndexOf('node_modules');
+  if (nmIdx === -1) {
+    return path.basename(file);
+  }
+  return parts.slice(nmIdx + 1).join('/');
+}
+
+const revealGeneratorCallback = () => {
+  const dirs = [
+    path.join(__dirname, '..', 'node_modules', 'reveal.js'),
+    path.join(cwd(), 'node_modules', 'reveal.js'),
+  ];
+  const dir = dirs.find((d) => fs.existsSync(d));
+  if (!dir) {
+    throw new Error('reveal.js not found');
+  }
+  const baseDir = fs.realpathSync(dir);
+
+  const seen = new Set();
+  const absoluteFiles = [];
+
+  for (const f of listFilesRecursive(baseDir)) {
+    if (!seen.has(f)) {
+      seen.add(f);
+      absoluteFiles.push(f);
+    }
+  }
+
+  const altReveal = path.join(cwd(), 'node_modules', 'revealjs');
+  if (fs.existsSync(altReveal)) {
+    for (const f of listFilesRecursive(altReveal)) {
+      if (!seen.has(f)) {
+        seen.add(f);
+        absoluteFiles.push(f);
+      }
+    }
+  }
+
+  return absoluteFiles.map((file) => ({
+    data: () => fs.createReadStream(file),
+    path: pathUnderNodeModules(file),
+  }));
 };
-exports.revealGeneratorCallback = revealGeneratorCallback;
+
+module.exports = { revealGeneratorCallback };
